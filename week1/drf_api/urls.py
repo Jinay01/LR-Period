75d83e3d20697a541af276394da447fd1ddeb123
Jinay <jinayparekh2001@gmail.com>
2020-09-10 02:08:51 +0530

    crud serializers

--- week1/drf_api/urls.py
+++ week1/drf_api/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # college section
     path('colleges', views.colleges, name='colleges'),
+    path('college-details/<str:pk>', views.collegeDetails, name="college-details"),
+    # stream section
     path('streams', views.streams, name='streams'),
+    path('stream-add', views.streamNew, name='stream-add'),
+    path('stream-update/<str:pk>', views.streamUpdate, name='stream-update'),
+    path('stream-delete/<str:pk>', views.streamDelete, name='stream-delete'),
 ]
--- week1/drf_api/views.py
+++ week1/drf_api/views.py
@@ -16,7 +16,38 @@ def colleges(request):
 
 
 @api_view(['GET'])
+def collegeDetails(request, pk):
+    college = College.objects.get(id=pk)
+    college_details = CollegeSerializer(college, many=False)
+    return Response(college_details.data)
+
+
+@api_view(['GET'])
 def streams(request):
     streams = Stream.objects.all()
     serializer = StreamSerializer(streams, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def streamNew(request):
+    serializer_form = StreamSerializer(data=request.data)
+    if serializer_form.is_valid():
+        serializer_form.save()
+    return Response(serializer_form.data)
+
+
+@api_view(['POST', 'GET'])
+def streamUpdate(request, pk):
+    stream = Stream.objects.get(id=pk)
+    serializer_form = StreamSerializer(instance=stream, data=request.data)
+    if serializer_form.is_valid():
+        serializer_form.save()
+    return Response(serializer_form.data)
+
+
+@api_view(['GET'])
+def streamDelete(request, pk):
+    stream = Stream.objects.get(id=pk)
+    stream.delete()
+    return Response('Stream deleted')
